9271f8671d96e7f08b2c2f95ab90d16723073dca
alejandrov44 <31495891+alejandrov44@users.noreply.github.com>
2025-03-08 00:00:28 +0100

    fix: add force-static dynamic

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,16 @@
 import type { MetadataRoute } from "next";
 import { url } from "@/utils/seo";
 
+export const dynamic = "force-static";
+
 const robots = (): MetadataRoute.Robots => {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${url}`,
+    sitemap: `${url}/sitemap.xml`,
   };
 };
+
 export default robots;
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { url } from "@/utils/seo";
 
+export const dynamic = "force-static";
+
 const sitemap = (): MetadataRoute.Sitemap => {
   return [
     {
