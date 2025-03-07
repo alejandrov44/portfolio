f07dcc574537c69525bfea7a7d9cab12f7c73a7a
alejandrov44 <31495891+alejandrov44@users.noreply.github.com>
2025-03-07 23:39:37 +0100

    fix: move robots and sitemap files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { url } from "./seo";
+import { url } from "@/utils/seo";
 
 const robots = (): MetadataRoute.Robots => {
   return {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { url } from "./seo";
+import { url } from "@/utils/seo";
 
 const sitemap = (): MetadataRoute.Sitemap => {
   return [
