5c1725bfffab65a9bd58ef70ecb62c76dca4ef04
alejandrov44 <31495891+alejandrov44@users.noreply.github.com>
2025-03-14 19:52:18 +0100

    ci: use static export

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ export const basePath = new URL(environmentVariables.baseUrl).pathname.replace(/
 
 export default () => {
   const nextConfig = {
+    output: "export",
     basePath: basePath !== "/" ? basePath : "",
     assetPrefix: basePath !== "/" ? basePath : "",
     trailingSlash: true,
@@ -16,31 +17,6 @@ export default () => {
     pageExtensions: ["tsx", "mdx"],
     experimental: {
       typedRoutes: true,
-    },
-    async headers() {
-      return [
-        {
-          source: "/(.*)",
-          headers: [
-            {
-              key: "Strict-Transport-Security",
-              value: "max-age=31536000; includeSubDomains; preload",
-            },
-            {
-              key: "X-Frame-Options",
-              value: "DENY",
-            },
-            {
-              key: "X-Content-Type-Options",
-              value: "nosniff",
-            },
-            {
-              key: "Referrer-Policy",
-              value: "strict-origin-when-cross-origin",
-            },
-          ],
-        },
-      ];
     },
   } satisfies NextConfig;
   return nextConfig;
